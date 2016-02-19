4a8140adb5b418b82f823ebd9ac9252bbf9f9ead
Thoronir42 <Thoronir42@Gmail.com>
2016-02-19 17:16:41 +0100

    Extended FileBrowser so that it creates file pointers too

--- src/sp_delostrelec/IO/FileBrowser.java
+++ src/sp_delostrelec/IO/FileBrowser.java
@@ -7,16 +7,26 @@ import java.io.File;
  * @author Stepan
  */
 public class FileBrowser {
-
+	private static final String FOLDER_TERRAINS = "resources/terrain_data";
+	
+	
 	public static String[] getTerrainFileNames() {
-		File f = new File("resources/terrain_data");
+		File f = new File(FOLDER_TERRAINS);
 		File[] files = f.listFiles((File dir, String name) -> name.endsWith(".ter"));
 
-		System.out.format("%s contains %d files. ", f.getAbsolutePath(), files.length);
+		System.out.format("%s contains %d files.\n", f.getAbsolutePath(), files.length);
 		String[] s = new String[files.length];
 		for (int i = 0; i < files.length; i++) {
 			s[i] = files[i].getName();
 		}
 		return s;
+	}
+
+	public static File openTerrainFile(String newValue) {
+		File f = new File(FOLDER_TERRAINS+"/"+newValue);
+		if(!f.exists()){
+			return null;
+		}
+		return f;
 	}
 }
